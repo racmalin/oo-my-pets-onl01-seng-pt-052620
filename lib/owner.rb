262aed320c8406a055e5babcd8516da80fcd24ab
Kevin C Malin <racmalin@yahoo.com>
2020-07-19 01:43:28 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,17 @@
 class Owner
-  # code goes here
+  @@all= []
+  attr_accessor :name, :pets
+  attr_reader :species
+
+
+
+  def initialize(species)
+    @species = species
+    @@all << self
+    @pets = {:dogs => [], :cats => []}
+  end
+
+  def say_species
+    return "I am a human."
+  end
 end
